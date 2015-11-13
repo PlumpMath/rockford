a1ae4d44eb0933196cc217e54cbc18e54257cbbd
Stu West <patabongo@gmail.com>
2015-11-13 21:29:27 +0000

    Delete db_credentials.clj

--- src/rockford/db_credentials.clj
+++
@@ -1,8 +0,0 @@
-(ns rockford.db-credentials)
-
-(def *items-spec* {:subprotocol "mysql"
-               :subname "//127.0.0.1:3306/items"
-               :user "root"
-               :password "xpass"})
-
-(def whatever "will git work this time")
