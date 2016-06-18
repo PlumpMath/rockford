e89865f7eb11024cd1cd9d7712a608d33fdc8b01
Stu West <patabongo@gmail.com>
2016-06-18 23:41:54 +0100

    Added MySQL file.

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ A web application for analysing drug resistance mutations from human pathogens. 
 
 I don't really imagine there are a lot of people out there who will find a use for this specific application, but while making it I've had to wrestle with various issues that the documentation doesn't directly address (How do you deal with enums in Clojure when doing Java interop? What happened to batch processing with prepared statements in clojure.java.jdbc >= 0.6? Was the concept of logging configuration files put on this earth to try us?) so potentially this code will help someone out there.
 
+## Setup
+
+Create a `rockford` MySQL database somewhere the app can access and import the supplied rockford.sql file. The default connection settings are in the `rockford.db.local` namespace; needless to say, these are hideously insecure and you should change them.
+
 ## Prerequisites
 
 You will need [Leiningen][] 2.0.0 or above installed.
---
+++ rockford.sql
@@ -0,0 +1,109 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.10)
+# Database: rockford
+# Generation Time: 2016-06-18 22:37:08 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table alignment
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `alignment`;
+
+CREATE TABLE `alignment` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `reference_id` int(11) unsigned DEFAULT NULL,
+  `alignment_name` varchar(100) DEFAULT NULL,
+  `results_filename` text,
+  `complete` tinyint(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table alignment_results
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `alignment_results`;
+
+CREATE TABLE `alignment_results` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `alignment_id` int(11) unsigned DEFAULT NULL,
+  `participant_id` int(11) unsigned DEFAULT NULL,
+  `dataset_id` int(11) unsigned DEFAULT NULL,
+  `sequence` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table consensus
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `consensus`;
+
+CREATE TABLE `consensus` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `alignment_id` int(11) unsigned DEFAULT NULL,
+  `header` text,
+  `filename` text,
+  `sequence` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table reference
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `reference`;
+
+CREATE TABLE `reference` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `reference_name` varchar(255) DEFAULT NULL,
+  `sequence` text,
+  `start_codon` int(11) DEFAULT NULL,
+  `end_codon` int(11) DEFAULT NULL,
+  `complete` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table reference_drms
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `reference_drms`;
+
+CREATE TABLE `reference_drms` (
+  `reference_id` int(11) unsigned NOT NULL,
+  `codon_id` int(11) NOT NULL,
+  `sequence` varchar(10) DEFAULT NULL,
+  `is_drm` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`reference_id`,`codon_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
